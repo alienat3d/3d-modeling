86fcaf8f8f9e18c1ba278ed03d360ae272484f7c
Al Zaplin <zaplin.dev@gmail.com>
2024-07-20 08:18:10 +0300

    feat(conditions for screen width) Appearing/Disappearing animations work only under width > 768px conditions

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,4 +1,5 @@
 export const modalFunc = () => {
+	const width = document.documentElement.clientWidth;
 	const modal = document.querySelector('.popup');
 	const buttons = document.querySelectorAll('.popup-btn');
 	const closeBtn = modal.querySelector('.popup-close');
@@ -6,7 +7,6 @@ export const modalFunc = () => {
 	let count = 0;
 	let idInterval;
 
-	modal.style.opacity = 0;
 	const modalAppearAnimation = () => {
 		idInterval = requestAnimationFrame(modalAppearAnimation);
 		if (count < 10) {
@@ -28,11 +28,17 @@ export const modalFunc = () => {
 		}
 	}
 
-	buttons.forEach(btn =>
-		btn.addEventListener('click', () => {
-			modal.style.display = 'block';
-			modalAppearAnimation();
-		}));
-
-	closeBtn.addEventListener('click', modalDisappearAnimation);
+	if (width > 768) {
+		modal.style.opacity = 0;
+		buttons.forEach(btn =>
+			btn.addEventListener('click', () => {
+				modal.style.display = 'block';
+				modalAppearAnimation();
+			}));
+		closeBtn.addEventListener('click', modalDisappearAnimation);
+	} else {
+		buttons.forEach(btn =>
+			btn.addEventListener('click', () => modal.style.display = 'block'));
+		closeBtn.addEventListener('click', () => modal.style.display = 'none');
+	}
 }
