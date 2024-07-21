95e0f10aca8091178aaae64922cc091f00ba1d7a
Al Zaplin <zaplin.dev@gmail.com>
2024-07-21 08:16:46 +0300

    feat(inputs validation) RegExp prevents typing any non-numbers... ...at work calculator inputs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { timerFunc } from "./modules/timer";
 import { menuFunc } from "./modules/menu";
 import { modalFunc } from "./modules/modal";
+import { calcFunc } from "./modules/calc";
 
 timerFunc('07 august 2024 14:00');
 menuFunc();
 modalFunc();
+calcFunc();
