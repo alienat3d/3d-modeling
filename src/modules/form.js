ef9ad3f7b9db13f89b109be608585b30621eb173
Al Zaplin <zaplin.dev@gmail.com>
2024-07-22 15:33:41 +0300

    feat(RegExp) Add addition RegExps to inputs for correcting several letters & symbols when losing focus

--- src/modules/form.js
+++ src/modules/form.js
@@ -17,15 +17,36 @@ export const formFunc = () => {
 	// const validEmail = /[\-\.[a-z]\_\!\~\*\']+@([a-z\-]+\.)+[a-z]{1,15}/g;
 	// const validPhone = /^[[\+\d{1,2}]8]\([\d]{3}\)[\d]{3}-[\d]{2}-[\d]{2}$/g;
 
+	/* 	const modifyInputs = (array, regExp) => {
+			array.forEach(input =>
+				input.addEventListener('input', () =>
+					input.value = input.value.replace(regExp, '')
+				)
+			);
+		} */
 	const modifyInputs = (array, regExp) => {
 		array.forEach(input =>
-			input.addEventListener('input', () =>
-				input.value = input.value.replace(regExp, '')
-			)
+			input.addEventListener('blur', () => {
+				input.value = input.value.replace(regExp, '');
+				input.value = input.value.replace(/[ ]{2,}/, ' ');
+				input.value = input.value.replace(/[-]{2,}/, '-');
+				input.value = input.value.replace(/^[ -]+/, '');
+				input.value = input.value.replace(/[ -]+$/, '');
+			})
+		);
+	}
+	const capitalizeFirstLetter = (array) => {
+		array.forEach(input =>
+			input.addEventListener('blur', () => {
+				input.value = input.value.replace(/^([а-яё])([а-яА-Яё]+)/g, (str, $1, $2) => {
+					return $1.toUpperCase() + $2.toLowerCase();
+				})
+			})
 		);
 	}
 
 	modifyInputs(textInputs, cyrillicLettersOnly);
+	capitalizeFirstLetter(textInputs);
 	modifyInputs(emailInputs, emailSymbolsOnly);
 	modifyInputs(phoneInputs, phoneSymbolsOnly);
 }
