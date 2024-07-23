ffbec01d0b3689dd2920b0e92710c2bb84c07fdb
Al Zaplin <zaplin.dev@gmail.com>
2024-07-23 10:06:25 +0300

    feat(tabs) Lesson #22 Changing tab-content by clicking on tabs

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import { menuFunc } from "./modules/menu";
 import { modalFunc } from "./modules/modal";
 import { calcFunc } from "./modules/calc";
 import { formFunc } from "./modules/form";
+import { tabsFunc } from "./modules/tabs";
 
 timerFunc('07 august 2024 14:00');
 menuFunc();
 modalFunc();
 calcFunc();
 formFunc();
+tabsFunc();
