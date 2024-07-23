ae0829ff68527f210b663fddadc9df09bce6e38e
Al Zaplin <zaplin.dev@gmail.com>
2024-07-23 17:07:46 +0300

    refactor(menu) Rewrote burger-menu functionality with event delegation

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,28 +1,32 @@
 export const menuFunc = () => {
-	const menuBtn = document.querySelector('.menu');
+	const main = document.querySelector('main');
 	const menu = document.querySelector('menu');
-	const closeMenuBtn = menu.querySelector('.close-btn');
-	const menuItems = menu.querySelectorAll('ul > li > a');
 	const scrollDownBtn = document.querySelector('a[href="#service-block"]');
 
-	const handleMenu = () => menu.classList.toggle('active-menu');
+	const handleMenu = () => {
+		menu.classList.toggle('active-menu');
+	}
 
-	scrollDownBtn.addEventListener('click', (evt) => {
-		evt.preventDefault();
-		const blockId = scrollDownBtn.getAttribute('href');
-		document.querySelector(blockId).scrollIntoView({ behavior: "smooth" });
-	})
-
-	menuBtn.addEventListener('click', handleMenu);
-	closeMenuBtn.addEventListener('click', handleMenu);
-
-	menuItems.forEach(item => {
-		item.addEventListener('click', (evt) => {
-			evt.preventDefault();
+	main.addEventListener('click', evt => {
+		if (evt.target.closest('.menu')) {
 			handleMenu();
-			const blockId = evt.target.getAttribute('href');
+		} else if (evt.target.closest('a[href="#service-block"]')) {
+			evt.preventDefault();
+			const blockId = scrollDownBtn.getAttribute('href');
 			document.querySelector(blockId).scrollIntoView({ behavior: "smooth" });
-		});
-	});
+		}
+	})
+	menu.addEventListener('click', (evt) => {
+		evt.preventDefault();
+
+		if (evt.target.closest('a[href^="#"]') && !evt.target.closest('a[href="#close"]')) {
+			const menuLink = evt.target;
+			const blockId = menuLink.getAttribute('href');
+			document.querySelector(blockId).scrollIntoView({ behavior: "smooth" });
+			handleMenu();
+		} else if (evt.target.closest('.close-btn')) {
+			handleMenu();
+		}
+	})
 }
 
