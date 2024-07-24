681865d9651b44467da7a93412e81d1d929fe47d
Al Zaplin <zaplin.dev@gmail.com>
2024-07-24 11:36:53 +0300

    feat(swiper JS) Carousel set and run for Clients section

---
+++ src/modules/swiperSettings.js
@@ -0,0 +1,40 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination, Autoplay, Keyboard } from 'swiper/modules';
+
+export const swiperSettingsFunc = () => {
+	const swiper = new Swiper('.swiper', {
+		modules: [ Navigation, Pagination, Autoplay, Keyboard ],
+		loop: true,
+		slidesPerView: 1,
+		spaceBetween: 10,
+		navigation: {
+			prevEl: '.swiper-button-prev',
+			nextEl: '.swiper-button-next',
+		},
+		pagination: {
+			el: ".swiper-pagination",
+			clickable: true,
+		},
+		keyboard: {
+			enabled: true,
+			onlyInViewport: true,
+		},
+		autoplay: {
+			delay: 2000,
+		},
+		breakpoints: {
+			640: {
+				slidesPerView: 2,
+				spaceBetween: 10,
+			},
+			768: {
+				slidesPerView: 4,
+				spaceBetween: 40,
+			},
+			1024: {
+				slidesPerView: 5,
+				spaceBetween: 50,
+			},
+		},
+	});
+}
